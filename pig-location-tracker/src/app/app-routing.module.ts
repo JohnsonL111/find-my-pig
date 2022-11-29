e1ceb341b72782ea883eb5fe595f34c94222b251
Johnson Luong <jkl53@sfu.ca>
2022-11-28 17:45:59 -0800

    Add basic bootstrap p

--- pig-location-tracker/src/app/app-routing.module.ts
+++ pig-location-tracker/src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PigAddFormComponent } from './components/pig-add-form/pig-add-form.component';
+import { PigLandingPageComponent } from './components/pig-landing-page/pig-landing-page.component';
 import { PigTableComponent } from './components/pig-table/pig-table.component';
 import { ReportMapComponent } from './components/report-map/report-map.component';
 
 
 // name does not matter as long as its typed by Routes (which is an array of Route interface objects)
 const routes: Routes = [
-  { path: '', component: PigTableComponent},
+  { path: '', component: PigLandingPageComponent},
   { path: 'add', component: PigAddFormComponent },
   //{ path: 'edit/:name', component: PersonEditFormComponent }
 ];
