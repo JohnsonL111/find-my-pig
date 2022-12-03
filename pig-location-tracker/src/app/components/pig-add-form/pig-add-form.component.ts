a188bb3d8c48dcd381349c472025a5e3b72d38d9
Johnson Luong <jkl53@sfu.ca>
2022-12-02 16:22:57 -0800

    Add empty field validation to form

--- pig-location-tracker/src/app/components/pig-add-form/pig-add-form.component.ts
+++ pig-location-tracker/src/app/components/pig-add-form/pig-add-form.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,9 +9,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./pig-add-form.component.css']
 })
 export class PigAddFormComponent implements OnInit{
+
   // router service and HttpClient is injected here
   constructor(private _router: Router, private _http: HttpClient) {
-    // if the user accesses this URL not through the "add button" on the main page then redirect them.
+    // add the disabled attribute back to the submit button
+    document.getElementById("submit")?.setAttribute("disabled", "disabled");
+
+    // if the user accesses this URL not through the "add button" on the main page then redirect them back.
     if (this._router.getCurrentNavigation()?.extras.state?.['legalNav'] == null) _router.navigateByUrl("")
   }
 
@@ -21,9 +26,54 @@ export class PigAddFormComponent implements OnInit{
       [], {hour: '2-digit', minute: '2-digit'});
   }
 
-  onClickFormSubmit(evt: Event) {
-    // do any input validation here
-    this._router.navigateByUrl("")
+  /**
+ *  Prevents a "-" from being entered for the ID
+ * @param e: the event object
+ */
+  preventNegatives(e: any) {
+    if (e.key === "-") {
+        e.preventDefault();
+        return false;
+    }
+    return true;
   }
 
+  validateForm() {
+    let submitButton = document.getElementById("submit");
+    console.log("form has been changed")
+    if (this.checkRequiredFields()) {
+      submitButton?.removeAttribute("disabled");
+    } else {
+      document.getElementById("submit")?.setAttribute("disabled", "disabled");
+    }
+  }
+
+  // checks if all inputs are filled out.
+  checkRequiredFields(): boolean {
+    return (this.addRequiredFieldsChecked() ? true : false)
+  }
+
+  // helper to check if all input fields have something in it
+  addRequiredFieldsChecked(): boolean {
+    let allInputs = document.querySelectorAll("input");
+    let allHasVal = true;
+    
+    allInputs.forEach((input) => {
+      if (input.value == "") {
+        allHasVal = false; 
+      }
+    }) 
+
+    return allHasVal;
+  }
+
+  // checks if the phone number returned matches the regex.
+  checkProperPhone() {
+    return true
+  }
+
+  // adds the pig report to the server and navigates back to the main screen
+  onClickFormSubmit() {
+    this._router.navigateByUrl("")
+  }
 }
